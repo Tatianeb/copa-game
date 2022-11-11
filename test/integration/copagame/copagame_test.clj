9a6a0f26b5e97bffafe2f0527daa786e6eb3d4e8
Tatiane Berrocal <tatiane.berrocal@180s.com.br>
2022-11-11 17:01:48 -0300

    PR correction

--- test/integration/copagame/copagame_test.clj
+++ test/integration/copagame/copagame_test.clj
@@ -25,6 +25,20 @@
     :router (components.router/new-router routes/routes)
     :webserver (component/using (components.webserver/new-webserver)
                                 [:config :http :router]))))
+
+(def teams
+  [{:id "/nintendo-64/the-legend-of-zelda-ocarina-of-time"
+    :titulo "The Legend of Zelda: Ocarina of Time (N64)"
+    :nota 99.9
+    :ano 1998,
+    :urlImagem "https://l3-processoseletivo.azurewebsites.net/api/CapaJogo/nintendo-64/the-legend-of-zelda-ocarina-of-time"},
+   {:id "/playstation/tony-hawks-pro-skater-2"
+    :titulo "Tony Hawk's Pro Skater 2 (PS)"
+    :nota 98.9
+    :ano 2000
+    :urlImagem "https://l3-processoseletivo.azurewebsites.net/api/CapaJogo/playstation/tony-hawks-pro-skater-2"}]
+  )
+
 (defflow
   flow-integration-copagame-test
   {:init (util/start-system! create-and-start-components!)
@@ -33,29 +47,11 @@
   (flow "should interact with system"
     (flow "prepare system with http-out mocks"
       (util.http/set-http-out-responses! {"https://l3-processoseletivo.azurewebsites.net/api/Competidores?copa=games"
-                                          {:body [{:id "/nintendo-64/the-legend-of-zelda-ocarina-of-time"
-                                                   :titulo "The Legend of Zelda: Ocarina of Time (N64)"
-                                                   :nota 99.9
-                                                   :ano 1998,
-                                                   :urlImagem "https://l3-processoseletivo.azurewebsites.net/api/CapaJogo/nintendo-64/the-legend-of-zelda-ocarina-of-time"},
-                                                  {:id "/playstation/tony-hawks-pro-skater-2"
-                                                   :titulo "Tony Hawk's Pro Skater 2 (PS)"
-                                                   :nota 98.9
-                                                   :ano 2000
-                                                   :urlImagem "https://l3-processoseletivo.azurewebsites.net/api/CapaJogo/playstation/tony-hawks-pro-skater-2"}]
+                                          {:body teams
                                            :status 200}})
 
       (flow "should list wallet deposits"
         (match? (matchers/embeds {:status 200
-                                  :body [{:id "/nintendo-64/the-legend-of-zelda-ocarina-of-time"
-                                          :titulo "The Legend of Zelda: Ocarina of Time (N64)"
-                                          :nota 99.9
-                                          :ano 1998,
-                                          :urlImagem "https://l3-processoseletivo.azurewebsites.net/api/CapaJogo/nintendo-64/the-legend-of-zelda-ocarina-of-time"},
-                                         {:id "/playstation/tony-hawks-pro-skater-2"
-                                          :titulo "Tony Hawk's Pro Skater 2 (PS)"
-                                          :nota 98.9
-                                          :ano 2000
-                                          :urlImagem "https://l3-processoseletivo.azurewebsites.net/api/CapaJogo/playstation/tony-hawks-pro-skater-2"}]})
+                                  :body teams})
                 (util.webserver/request! {:method :get
                                           :uri    "/copagame/read"}))))))
